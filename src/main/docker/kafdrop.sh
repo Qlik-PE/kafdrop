b04c365fa58bd40471d8df955ac554029ab4a373
John Neal <john.neal@qlik.com>
2021-05-26 14:21:07 -0500

    fixed bugs in docker config

--- src/main/docker/kafdrop.sh
+++ src/main/docker/kafdrop.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
-echo ""
-echo ":::    :::     :::     :::::::::: :::::::::  :::::::::   ::::::::  :::::::::        ::::::::  "
-echo ":+:   :+:    :+: :+:   :+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+:      :+:    :+: "
-echo "+:+  +:+    +:+   +:+  +:+        +:+    +:+ +:+    +:+ +:+    +:+ +:+    +:+             +:+ "
-echo "+#++:++    +#++:++#++: :#::+::#   +#+    +:+ +#++:++#:  +#+    +:+ +#++:++#+           +#++:  "
-echo "+#+  +#+   +#+     +#+ +#+        +#+    +#+ +#+    +#+ +#+    +#+ +#+                    +#+ "
-echo "#+#   #+#  #+#     #+# #+#        #+#    #+# #+#    #+# #+#    #+# #+#             #+#    #+# "
-echo "###    ### ###     ### ###        #########  ###    ###  ########  ###              ########  "
-echo ""
+echo "Starting Kafdrop"
 
 # Set marathon ports to 0:0 to have marathon assign and pass random port
-if [ $PORT0 ]; then
+if [ "$PORT0" ]; then
     JMX_PORT=$PORT0;
 fi
 
 # Marathon passes "HOST" variable
-if [ -z $HOST ]; then
+if [ -z "$HOST" ]; then
     HOST=localhost;
 fi
 
 # Marathon passes memory limit
-if [ $MARATHON_APP_RESOURCE_MEM ]; then
+if [ "$MARATHON_APP_RESOURCE_MEM" ]; then
     HEAP_ARGS="-Xms${MARATHON_APP_RESOURCE_MEM%.*}m -Xmx${MARATHON_APP_RESOURCE_MEM%.*}m"
 fi
 
-if [ $JMX_PORT ]; then
+if [ "$JMX_PORT" ]; then
     JMX_ARGS="-Dcom.sun.management.jmxremote \
     -Dcom.sun.management.jmxremote.port=${JMX_PORT} \
     -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT} \
@@ -37,31 +29,44 @@ fi
 
 KAFKA_PROPERTIES_FILE=${KAFKA_PROPERTIES_FILE:-kafka.properties}
 if [ "$KAFKA_PROPERTIES" != "" ]; then
-  echo Writing Kafka properties into $KAFKA_PROPERTIES_FILE
-  echo "$KAFKA_PROPERTIES" | base64 --decode --ignore-garbage > $KAFKA_PROPERTIES_FILE
+  echo "Writing Kafka properties into $KAFKA_PROPERTIES_FILE"
+  echo "$KAFKA_PROPERTIES" | base64 --decode --ignore-garbage > "$KAFKA_PROPERTIES_FILE"
 else
-  rm $KAFKA_PROPERTIES_FILE |& > /dev/null | true
+  rm -f "$KAFKA_PROPERTIES_FILE"
 fi
 
 KAFKA_TRUSTSTORE_FILE=${KAFKA_TRUSTSTORE_FILE:-kafka.truststore.jks}
 if [ "$KAFKA_TRUSTSTORE" != "" ]; then
-  echo Writing Kafka truststore into $KAFKA_TRUSTSTORE_FILE
-  echo "$KAFKA_TRUSTSTORE" | base64 --decode --ignore-garbage > $KAFKA_TRUSTSTORE_FILE
+  echo "Writing Kafka truststore into $KAFKA_TRUSTSTORE_FILE"
+  echo "$KAFKA_TRUSTSTORE" | base64 --decode --ignore-garbage > "$KAFKA_TRUSTSTORE_FILE"
 else
-  rm $KAFKA_TRUSTSTORE_FILE |& > /dev/null | true
+  rm -f "$KAFKA_TRUSTSTORE_FILE"
 fi
 
 KAFKA_KEYSTORE_FILE=${KAFKA_KEYSTORE_FILE:-kafka.keystore.jks}
 if [ "$KAFKA_KEYSTORE" != "" ]; then
-  echo Writing Kafka keystore into $KAFKA_KEYSTORE_FILE
-  echo "$KAFKA_KEYSTORE" | base64 --decode --ignore-garbage > $KAFKA_KEYSTORE_FILE
+  echo "Writing Kafka keystore into $KAFKA_KEYSTORE_FILE"
+  echo "$KAFKA_KEYSTORE" | base64 --decode --ignore-garbage > "$KAFKA_KEYSTORE_FILE"
 else
-  rm $KAFKA_KEYSTORE_FILE |& > /dev/null | true
+  rm -f "$KAFKA_KEYSTORE_FILE" 
 fi
+
+username=${USERNAME:-admin}
+password=${PASSWORD:-admin}
+role=${PASSWORD:-USER}
 
 ARGS="--add-opens=java.base/sun.nio.ch=ALL-UNNAMED -Xss256K \
      $JMX_ARGS \
      $HEAP_ARGS \
      $JVM_OPTS"
 
-exec java $ARGS -jar /kafdrop*/kafdrop*jar ${CMD_ARGS}
+CMD_ARGS="$CMD_ARGS \
+         --spring.security.user.name=$username \
+         --spring.security.user.password=$password \
+         --spring.security.user.roles=$role"
+
+CMD="exec java $ARGS -jar /kafdrop*.jar ${CMD_ARGS}"
+echo "exec java: $CMD"
+
+${CMD}
+
